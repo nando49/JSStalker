7054a72aaa1deb82fe55c7981e4e7119e7b6de0e
Nandonator <fernando.cuevas.uw@gmail.com>
2019-01-12 22:49:14 -0800

    This is an example

---
+++ script.js
@@ -0,0 +1,5 @@
+var userFirstName = prompt("Please enter your first name");
+var userSecondLast = prompt("Please enter your second and last name");
+var userAge = prompt("Please enter user age");
+alert(" User's full nameL : " + userFirstName + " " + userSecondLast+ " and age is: " + userAge);
+console.log(" User's full name : " + userFirstName + " " + userSecondLast + " and age is: " + userAge);
